ed1b114f41c20b26086c7626485f9c42b5bc3825
Suyash Agrawal <ozym4nd145@outlook.com>
2017-06-16 10:42:16 +0530

    removed unnecessary dependancy of mongoose

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
 var AWS = require("aws-sdk");
 var passport = require('passport');
 var flash    = require('connect-flash');
